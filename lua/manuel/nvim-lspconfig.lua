757c944d90f90de84b383e816e3716b612c4958d
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-11-29 17:40:50 +0100

    Add go language server to lsp config

--- lua/manuel/nvim-lspconfig.lua
+++ lua/manuel/nvim-lspconfig.lua
@@ -11,16 +11,39 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'sd', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 end
 
-local servers = { 'cssls', 'tsserver', 'dockerls', 'html', 'svelte' }
+require('lspconfig').tsserver.setup({
+  capabilities = capabilities,
+  on_attach = on_attach
+})
 
-for _, lsp in pairs(servers) do
-  require('lspconfig')[lsp].setup {
-    capabilities = capabilities,
-    on_attach = on_attach,
-  }
-end
+require('lspconfig').cssls.setup({
+  capabilities = capabilities,
+  on_attach = on_attach
+})
 
-local cmp = require'cmp'
+require('lspconfig').html.setup({
+  capabilities = capabilities,
+  on_attach = on_attach
+})
+
+require('lspconfig').dockerls.setup({
+  capabilities = capabilities,
+  on_attach = on_attach
+})
+
+require('lspconfig').svelte.setup({
+  capabilities = capabilities,
+  on_attach = on_attach
+})
+
+require('lspconfig').gopls.setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+  cmd = {'gopls', 'serve'},
+})
+
+-- Setup nvim-cmp
+local cmp = require('cmp')
 
 cmp.setup({
   snippet = {
@@ -38,7 +61,6 @@ cmp.setup({
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
-  }, {
     { name = 'buffer' },
   })
 })
