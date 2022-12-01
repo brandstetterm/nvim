0177ea6104cd801f49c7bf2472746724844cafbb
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-12-01 22:51:13 +0100

    Add icon to lualine lsp server

--- lua/manuel/lualine.lua
+++ lua/manuel/lualine.lua
@@ -5,21 +5,24 @@ require('lualine').setup({
     lualine_c = {'filename'},
     lualine_x = {'encoding'},
     lualine_y = {
-      function()
-        local msg = 'No Active Lsp'
-        local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-        local clients = vim.lsp.get_active_clients()
-        if next(clients) == nil then
-          return msg
-        end
-        for _, client in ipairs(clients) do
-          local filetypes = client.config.filetypes
-          if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-            return client.name
+      {
+        function()
+          local msg = '/'
+          local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+          local clients = vim.lsp.get_active_clients()
+          if next(clients) == nil then
+            return msg
           end
-        end
-        return msg
-      end,
+          for _, client in ipairs(clients) do
+            local filetypes = client.config.filetypes
+            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+              return client.name
+            end
+          end
+          return msg
+        end,
+        icon = ' ',
+      },
     },
     lualine_z = {'location'},
   }
