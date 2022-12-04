05de7817e92a313c453510b21782eab726ab04d9
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-12-04 19:48:18 +0100

    Try lsp-zero

--- lua/manuel/nvim-lspconfig.lua
+++ lua/manuel/nvim-lspconfig.lua
@@ -1,87 +1,40 @@
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+local lsp = require("lsp-zero")
 
-local opts = { noremap=true, silent=true }
+lsp.preset("recommended")
 
-local on_attach = function(client, bufnr)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'sd', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-end
-
-require('lspconfig').tsserver.setup({
-  capabilities = capabilities,
-  on_attach = on_attach
+lsp.ensure_installed({
+  "tsserver",
+  "cssls",
+  "html",
+  "dockerls",
+  "svelte",
+  "sumneko_lua",
+  "gopls"
 })
 
-require('lspconfig').cssls.setup({
-  capabilities = capabilities,
-  on_attach = on_attach
-})
-
-require('lspconfig').html.setup({
-  capabilities = capabilities,
-  on_attach = on_attach
-})
-
-require('lspconfig').dockerls.setup({
-  capabilities = capabilities,
-  on_attach = on_attach
-})
-
-require('lspconfig').svelte.setup({
-  capabilities = capabilities,
-  on_attach = on_attach
-})
-
-require('lspconfig').sumneko_lua.setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { 'vim' }
-      }
-    }
-  }
-})
-
-require('lspconfig').gopls.setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
-  cmd = {'gopls', 'serve'},
-})
-
--- Setup nvim-cmp
 local cmp = require('cmp')
-
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body) 
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-j>'] = cmp.mapping.select_next_item(),
-    ['<C-k>'] = cmp.mapping.select_prev_item(),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'buffer' },
-  })
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-k>'] = cmp.mapping.select_prev_item(),
+  ['<C-j>'] = cmp.mapping.select_next_item(),
+  ['<C-Space>'] = cmp.mapping.complete(),
 })
 
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-  }, {
-    { name = 'buffer' },
-  })
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
 })
+
+lsp.on_attach(function(client, bufnr) 
+  local opts = {buffer = bufnr, remap = false}
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("n", "sd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prevt() end, opts)
+end)
+
+lsp.nvim_workspace()
+lsp.setup()
+
+
+
--- lua/manuel/packer.lua
+++ lua/manuel/packer.lua
@@ -1,19 +1,41 @@
 return require('packer').startup(function()
   use('wbthomason/packer.nvim')
+  use('nvim-lua/plenary.nvim')
+  use('nvim-telescope/telescope.nvim')
+
+  -- Colorscheme
   use({'catppuccin/nvim', as = 'catppuccin'})
+
+  -- Syntax Highlighting 
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+  -- Statusline
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+  -- File tree
   use({'nvim-tree/nvim-tree.lua', requires = {'nvim-tree/nvim-web-devicons'}})
+
+  -- Git
   use('tpope/vim-fugitive')
-  use('nvim-lua/plenary.nvim')
-  use('nvim-telescope/telescope.nvim')
+
+  -- LSP
+  use("VonHeikemen/lsp-zero.nvim")
   use('neovim/nvim-lspconfig')
+  use('williamboman/mason.nvim')
+  use('williamboman/mason-lspconfig.nvim')
+
+  -- Autocomplete
   use("hrsh7th/nvim-cmp")
-  use("hrsh7th/cmp-nvim-lsp")
   use("hrsh7th/cmp-buffer")
-  use('L3MON4D3/LuaSnip')
+  use("hrsh7th/cmp-path")
   use('saadparwaiz1/cmp_luasnip')
+  use("hrsh7th/cmp-nvim-lsp")
+  use("hrsh7th/cmp-nvim-lua")
+
+  -- Snippets
+  use('L3MON4D3/LuaSnip')
+  use("rafamadriz/friendly-snippets")
 end)
